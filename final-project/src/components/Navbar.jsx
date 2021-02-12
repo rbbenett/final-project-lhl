5162cacd50ba06ba2aaf79f37fa2ed8a865aea7f
Lucas Kitteridge <lucas@MacBook.local>
2021-02-12 18:01:47 -0500

    components from src added and ness.css and bootstap

---
+++ final-project/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div class="nes-container with-title is-centered">
+        <p class="title"></p>
+        <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
