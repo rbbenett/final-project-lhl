27ca1a794d58a9daca7352984e5b1f0db4db4a99
Saad Kureshi <saq619@gmail.com>
2021-02-12 11:41:23 -0500

    seeds and schema added

---
+++ final-project-api/db/schema/00_all_migrations.sql
@@ -0,0 +1,33 @@
+-- Drop and recreate tables
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS levels CASCADE;
+DROP TABLE IF EXISTS attempts CASCADE;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  avatar BYTEA,
+  city VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  words_per_min INTEGER DEFAULT 0,
+  highest_level_cleared INTEGER DEFAULT 0
+);
+
+CREATE TABLE levels (
+  id SERIAL PRIMARY KEY NOT NULL,
+  content TEXT NOT NULL,
+  time_permitted INTEGER NOT NULL DEFAULT 30
+)
+
+CREATE TABLE attempts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  level_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  words_completed INTEGER NOT NULL,
+  time_taken NUMERIC NOT NULL,
+  passed BOOLEAN,
+  attempted_at TIMESTAMP DEFAULT NOW()
+)
