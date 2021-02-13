87771f48ea04b445938dfc708ac955980a2161c6
Saad Kureshi <saq619@gmail.com>
2021-02-12 21:28:00 -0500

    styling updated

--- final-project/src/components/Navbar.jsx
+++ final-project/src/components/Navbar.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import "nes.css/css/nes.min.css";
+// import "nes.css/css/nes.min.css";
 import "./Navbar.css"
-
-import Navbar from 'react-bootstrap/Navbar';
 
 function Navbar() {
   return (
     <div>
-      
+
     </div>
   )
 }
