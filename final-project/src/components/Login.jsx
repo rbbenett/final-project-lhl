68969a19dc7dfd9e7a17e420d0b317f06691374c
Robert <rbbenett0614@gmail.com>
2021-02-13 11:44:28 -0500

    Updated Register Form

--- final-project/src/components/Login.jsx
+++ final-project/src/components/Login.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import "nes.css/css/nes.min.css";
 import "./Navbar.css"
 
 import Navbar from 'react-bootstrap/Navbar';
@@ -12,18 +11,7 @@ function Login() {
   return (
     <div>
       <>
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>
-          </Form>
-        </Navbar>
+       
       </>
     </div>
   )
