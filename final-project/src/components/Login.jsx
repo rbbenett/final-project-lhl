f2768582a57b90b1bb234a7869dbb427270b93f8
Robert <rbbenett0614@gmail.com>
2021-02-13 11:56:22 -0500

    Created Login Template

--- final-project/src/components/Login.jsx
+++ final-project/src/components/Login.jsx
@@ -6,13 +6,51 @@ import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
+import Container from 'react-bootstrap/Container'
 
 function Login() {
   return (
-    <div>
-      <>
-       
-      </>
+    <div className="login">
+      <Container>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Form class="search" inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar>
+      </Container>
+      <Form className="register-form">
+        <Form.Group controlId="formHeader">
+          <h3>Login</h3>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="username" placeholder="Enter Username" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+          <Form.Control
+            type="password"
+            id="inputPassword5"
+            aria-describedby="passwordHelpBlock"
+            placeholder="Enter Password"
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formSubmitButton">
+          <Button variant="primary" type="submit">
+            Submit
+        </Button>
+        </Form.Group>
+      </Form>
     </div>
   )
 }
