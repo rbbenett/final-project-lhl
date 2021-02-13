b94960d0933eedf5366eb02e6c6a92a040182e7d
Saad Kureshi <saq619@gmail.com>
2021-02-13 16:31:52 -0500

    seeds updated

--- final-project-api/db/seeds/01_users_seeds.sql
+++ final-project-api/db/seeds/01_users_seeds.sql
@@ -1,7 +1,8 @@
-INSERT INTO users (username, email, password, avatar, city, country, words_per_min, highest_level_cleared)
-VALUES (Jsmith, jsmith@gmail.com, password, null, Toronto, Canada, 66.73, 5),
-(ChuckNorr, ChuckNorris@hotmail.com, redhead, null, Los Angeles, USA, 43.42, 3),
-(Tfey, TinaFey@gmail.com, 30rock, null, New York City, USA, 12, 0),
-(CChanning, carol@channing.ca, outofmymind, null, Montreal, Canada, 39.85, 3),
-(Cdion, Celine@dion.ca, Myheartwill, null, Montreal, Canada, 102.90, 10),
-(Tfox, terry@fox.com, sweetcharity, null, Toronto, Canada, 0, 0)
+INSERT INTO users (username, first_name, last_name, email, password, avatar, city, country, words_per_min, highest_level_cleared)
+VALUES 
+('Jsmith', 'Jaden', 'Smith', 'jsmith@gmail.com', password, null, 'Toronto', 'Canada', 66.73, 5),
+('ChuckNorr', 'Chuck', 'Norris', 'ChuckNorris@hotmail.com', 'redhead', null, 'Los Angeles', 'USA', 43.42, 3),
+('Tfey', 'Tina', 'Fey', 'TinaFey@gmail.com', '30rock', null, 'New York City', 'USA', 12, 0),
+('CChanning', 'Carol', 'Channing', 'carol@channing.ca', 'outofmymind', null, 'Montreal', 'Canada', 39.85, 3),
+('Cdion', 'Celine, Dion', 'Celine@dion.ca', 'Myheartwill', null, 'Montreal', 'Canada', 102.90, 10),
+('Tfox', 'terry@fox.com', 'sweetcharity', null, 'Toronto', 'Canada', 0, 0)
