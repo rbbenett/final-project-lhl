7d02bce47e3635879536d0c1b760f00c00aceb65
Saad Kureshi <saq619@gmail.com>
2021-02-13 15:39:17 -0500

    profile page added

--- final-project/src/components/Profile.jsx
+++ final-project/src/components/Profile.jsx
@@ -5,24 +5,54 @@ import "./Profile.css"
 function Profile() {
   return (
     <div className="profile">
-      <Card border="secondary" style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="images/sample-avatar.jpg" className="avatar" />
-        <Card.Body>
-          <Card.Title>Bobby Bob</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">bobthebuilder99</Card.Subtitle>
-          <ProgressBar animated now={70} label="70%" />
-          {/* <Card.Text><i class="fas fa-certificate"></i> Veteran</Card.Text> */}
-          <Card.Text>
+      <Container>
+        <Row>
+          <Col>
+            <Card border="secondary" style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="images/sample-avatar.jpg" className="avatar" />
+              <Card.Body>
+                <Card.Title>Bobby Bob</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">bobthebuilder99</Card.Subtitle>
+                <ProgressBar animated now={70} label="70%" />
+                {/* <Card.Text><i class="fas fa-certificate"></i> Veteran</Card.Text> */}
+                <Card.Text>
+                  <br/>
+                  <Card.Img variant="top" src="images/medal-icon.png" className= "medal-icon" />
+                  Veteran
+                </Card.Text>
+                <Card.Text>Highest Level Completed: 4</Card.Text>
+                <Card.Text>Average WPM: 97</Card.Text>
+                <Button variant="primary">Global Leaderboard</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Row>
+              <Card style={{ width: '45rem' }} >
+                <Card.Header as="h5">Edit Profile</Card.Header>
+                <Card.Body>
+                  <Card.Title>Special title treatment</Card.Title>
+                  <Card.Text>Change username</Card.Text>
+                  <Card.Text>Change location</Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Row>
             <br/>
-            <Card.Img variant="top" src="images/medal-icon.png" className= "medal-icon" />
-             Veteran
-          </Card.Text>
-          <Card.Text>Highest Level Completed: 4</Card.Text>
-          <Card.Text>Average WPM: 97</Card.Text>
-          <Button variant="primary">Global Leaderboard</Button>
-        </Card.Body>
-        {/* <Card.Body>This is some text within a card body.</Card.Body> */}
-    </Card>
+            <br/>
+            <Row>
+              <Card style={{ width: '45rem' }} >
+                <Card.Header as="h5">Security</Card.Header>
+                <Card.Body>
+                  <Card.Title>Special title treatment</Card.Title>
+                  <Card.Text>Change password</Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
