a7ce05ca8636c9282f8520d5d894f3ac5e33e0c5
Robert <rbbenett0614@gmail.com>
2021-02-12 20:09:05 -0500

    Started registration form

--- final-project/src/components/Register.jsx
+++ final-project/src/components/Register.jsx
@@ -9,14 +9,19 @@ function Register() {
   return (
     <div className="register">
       <Form>
+      <Form.Group controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Enter Name" />
+        </Form.Group>
+
         <Form.Group controlId="formBasicUsername">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="username" placeholder="Enter username" />
+          <Form.Control type="username" placeholder="Enter Username" />
         </Form.Group>
 
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter Email" />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -26,9 +31,20 @@ function Register() {
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+
+        <Form.Group controlId="formBasicPasswordConfirmation">
+          <Form.Label>Password Confirmation</Form.Label>
+          <Form.Control type="password" placeholder="Password Confirmation" />
         </Form.Group>
+
+        <Form.Group>
+          <Form.File id="exampleFormControlFile1" label="Avatar" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Accept terms & conditions" />
+        </Form.Group>
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
