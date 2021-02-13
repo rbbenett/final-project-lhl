d008094c4fc070bebc784ea21326b54304eb09d6
Robert <rbbenett0614@gmail.com>
2021-02-13 15:26:41 -0500

    Adding showForm

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,12 +1,49 @@
 import './App.css';
-
+import React, { Component } from 'react';
 import Register from "./components/Register.jsx";
 import Footer from "./components/Footer.jsx";
 import Login from "./components/Login.jsx";
 import Profile from "./components/Profile.jsx";
 import {Navbar, Nav} from 'react-bootstrap';
 
-function App() {
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    // Here we initialize our components state
+    this.state = {
+        showForm: false
+    };
+
+    this.onLoginClick = this.onLoginClick.bind(this);
+    this.onRegisterClick = this.onRegisterClick.bind(this);
+}
+
+onLoginClick () {
+    // On click we change our state – this will trigger our `render` method
+    this.setState({ showForm: true });
+}
+
+onRegisterClick () {
+  // On click we change our state – this will trigger our `render` method
+  this.setState({ showForm: true });
+}
+
+renderLoginForm () {
+   return (
+       <Login />
+   );
+}
+
+renderRegisterForm () {
+  return (
+      <Register />
+  );
+}
+
+  
+  render(){
+    const { showForm } = this.state;
   return (
     <div className="app">
       {/* if not loggin in */}
@@ -15,8 +52,8 @@ function App() {
           <Nav className="mr-auto">
           </Nav>
           <Nav>
-            <Nav.Link href="#login">Login</Nav.Link>
-            <Nav.Link eventKey={2} href="#register">Register</Nav.Link>
+          <Nav.Link onClick={ this.onLoginClick }>Login</Nav.Link>
+          <Nav.Link onClick={ this.onRegisterClick }>Register</Nav.Link>
           </Nav>
       </Navbar>
       {/* Once logged in */}
@@ -30,12 +67,12 @@ function App() {
             <Nav.Link eventKey={2} href="#logout">Logout</Nav.Link>
           </Nav>
       </Navbar>
-      <Login />
-      <Register />
+      {showForm && this.renderLoginForm() || this.renderRegisterForm()}
       <Profile />
       {/* <Footer /> */}
     </div>
   );
+}
 }
 
 export default App;
