0d64b2976d536113a32503221505268d73a84479
Robert <rbbenett0614@gmail.com>
2021-02-12 21:28:41 -0500

    Reverted back to bootstrap

--- final-project/src/components/Main.jsx
+++ final-project/src/components/Main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Register from "./components/Register"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
-import "nes.css/css/nes.min.css";
 
 function Main() {
   return (
