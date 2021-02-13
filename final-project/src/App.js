29dc74a52d3fcc2df95204aa61bbb239e6698009
Saad Kureshi <saq619@gmail.com>
2021-02-12 21:28:00 -0500

    styling updated

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,10 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import Register from "./components/Register.jsx";
 import Footer from "./components/Footer.jsx";
 import Login from "./components/Login.jsx";
-// import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
