6e0ea27b273b08fbb0f6b276a9149db36b2ef504
Saad Kureshi <saq619@gmail.com>
2021-02-13 11:54:35 -0500

    navbar style updated

--- final-project/src/components/Register.jsx
+++ final-project/src/components/Register.jsx
@@ -12,20 +12,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Register() {
   return (
     <div className="register">
-      <Container>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Form class="search" inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-      </Navbar>
-      </Container>
       <Form className="register-form">
         <Form.Group controlId="formHeader">
         <h3>Register</h3>
