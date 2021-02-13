42c2479e39b840434ae55eee8f80a602729dbe73
Saad Kureshi <saq619@gmail.com>
2021-02-13 13:39:43 -0500

    profile card updated

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Register from "./components/Register.jsx";
 import Footer from "./components/Footer.jsx";
 import Login from "./components/Login.jsx";
+import Profile from "./components/Profile.jsx";
 import {Navbar, Nav} from 'react-bootstrap';
 
 function App() {
