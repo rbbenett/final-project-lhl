73a6d184f95da5bbe1dcce06269b55c015539f61
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-02-14 16:20:55 -0500

    begining of game logic, still working on implementing time

--- final-project/src/components/GameConsole.jsx
+++ final-project/src/components/GameConsole.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
 import "./GameConsole.css"
+import Countdown from "react-countdown";
 import { Jumbotron, Button, ProgressBar, Spinner, InputGroup, FormControl } from 'react-bootstrap';
 
 
 function GameConsole() {
+
+  const Completionist = () => <span>GameOver</span>;
+
+// Renderer callback with condition
+const renderer = ({ seconds, completed }) => {
+  if (completed) {
+    postToAttempts(user_id, level_id, paragraph.length, 30, false)
+    // Render a complete state
+      .then( render (<Completionist />)
+        )
+    
+  } else {
+    // Render a countdown
+    return (
+      <span id="countdown">
+        {seconds}
+      </span>
+    );
+  }
+};
+
+ReactDOM.render(
+  <Countdown date={Date.now() + 30000} renderer={renderer} />,
+);
+
+  const checkingForMatch = function(textarea, paragraph) {
+  if (document.getElementById('textarea').value === paragraph){
+    stop()
+    const timerValue = document.getElementById('countdown').value
+    .then(
+      postToAttempts(user_id, level_id, paragraph.length, timerValue, true)
+    )
+    .then(//pull data from the database about what level you're on then render the gameplay
+      )
+    }
+  }
+
+  const postToAttempts = function(userId, levelId, wordsCompleted, secondsOnTimer, passOrFail){
+    return pool.query(`
+    INSERT INTO attempts (user_id, level_id, words_completed, time_taken, passed)
+    VALUES($1, $2, $3, $4, $5)
+    RETURNING *;`, [userId, levelId, wordsCompleted, secondsOnTimer, passOrFail])
+  }
+
   return (
     <div className="gameconsole">
       <Jumbotron>
@@ -34,7 +79,7 @@ function GameConsole() {
         </p>
         <InputGroup>
           <InputGroup.Prepend>
-            <InputGroup.Text>TYPE HERE:</InputGroup.Text>
+            <InputGroup.Text onKeyUp="checkingforMatch()" >TYPE HERE:</InputGroup.Text>
           </InputGroup.Prepend>
           <FormControl as="textarea" aria-label="With textarea" />
         </InputGroup>
