d1903db2fb42d633c69b41973bab917e9034d386
Robert <rbbenett0614@gmail.com>
2021-02-14 15:31:28 -0500

    Updating users route to api

--- final-project-api/routes/users.js
+++ final-project-api/routes/users.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/users', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
-module.exports = router;
+// module.exports = router;
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
+
