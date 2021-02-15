4d7a6bbf3d63a4c5a7b95a59d49ed52fbec7be36
Saad Kureshi <saq619@gmail.com>
2021-02-15 01:44:28 -0500

    helper functions added

--- final-project/src/hooks/useApplicationData.js
+++ final-project/src/hooks/useApplicationData.js
@@ -2,31 +2,78 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
-  const [state, setState] = useState({
-    newUserDetails: {
-      firstName: "",
-      lastName: "",
-      userName: "",
-      email: "",
-      password: "",
-      passwordConfirmation: "",
-      avatar: null
-    },
-    levels: {},
-    contents: {}
-  })
 
+  // All States
+  const [levels, setLevels] = useState([]);
+  const [contents, setContents] = useState([]);
+  const [newUserDetails, setNewUserDetails] = useState({
+    username: "", 
+    first_name: "", 
+    last_name: "", 
+    email: "", 
+    password: "", 
+    avatar: "", 
+    city: "", 
+    country: ""
+  });
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
+  // All helper functions
+  const handleCloseLogin = () => setShowLogin(false);
+  const handleShowLogin = () => setShowLogin(true);
+  const handleCloseRegister = () => setShowRegister(false);
+  const handleShowRegister = () => setShowRegister(true);
+  const registerUser = () => {
+    axios.post('http://localhost:3004/api/users', {
+      username: newUserDetails.username, 
+      first_name: newUserDetails.first_name, 
+      last_name: newUserDetails.last_name, 
+      email: newUserDetails.email, 
+      password: newUserDetails.password, 
+      avatar: newUserDetails.avatar, 
+      city: newUserDetails.city, 
+      country: newUserDetails.country
+    })
+    .then(res => {
+      console.log(res);
+    })
+  }
+  const loadLevelOne = contentsArray => {
+    return contentsArray[0].content;
+  }
+
+  // To update state from db on load
   useEffect(() => {
     Promise.all([
       axios.get("http://localhost:3004/api/contents"),
       axios.get("http://localhost:3004/api/levels")
-    ]).then((all) => {
-      setState(prev => ({...prev, contents: all[0].data, levels: all[1].data }));
-      console.log("content looks like >>", state.contents);
-    }).catch((err) => {
-      console.log("LOLOLOLOL", err);
+    ])
+    .then(res => {
+      let contentsArray = res[0].data["contents"];
+      let levelsArray = res[1].data["levels"];
+      setContents(contentsArray);
+      setLevels(levelsArray);
     })
+    .catch((err) => console.log(err))
   }, [])
 
-  return state;
+  return {
+    showLogin,
+    setShowLogin,
+    showRegister,
+    setShowRegister,
+    handleCloseLogin,
+    handleShowLogin,
+    handleCloseRegister,
+    handleShowRegister,
+    levels,
+    setLevels,
+    contents,
+    setContents,
+    loadLevelOne,
+    newUserDetails,
+    setNewUserDetails,
+    registerUser
+  };
 }
