62cc8257228acc9185769ac73f8723f3926ce5a2
Robert <rbbenett0614@gmail.com>
2021-02-16 16:45:48 -0500

    Cleaned up code

--- final-project/src/components/EditUser.jsx
+++ final-project/src/components/EditUser.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { Form, Button, Col, InputGroup } from 'react-bootstrap'
 import "./EditUser.css"
