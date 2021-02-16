4b40952900ff23cd6573e3c5bb86a3d88d460cbb
saadkureshi <saq619@gmail.com>
2021-02-16 18:14:57 +0000

    local storage for user details implemented, refactor of navbar

--- final-project/src/hooks/useApplicationData.js
+++ final-project/src/hooks/useApplicationData.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Prev } from "react-bootstrap/esm/PageItem";
 
 export default function useApplicationData() {
 
@@ -9,47 +8,8 @@ export default function useApplicationData() {
   const [contents, setContents] = useState([]);
   const [attempts, setAttempts] = useState([]);
   const [users, setUsers] = useState([]);
-  const [gameConsole, setGameConsole] = useState('');
   const [userInput, setUserInput] = useState('');
-  // const [newUserDetails, setNewUserDetails] = useState({
-  //   username: "",
-  //   first_name: "",
-  //   last_name: "",
-  //   email: "",
-  //   password: "",
-  //   avatar: "",
-  //   city: "",
-  //   country: ""
-  // });
-  const [showLogin, setShowLogin] = useState(false);
-  const [showRegister, setShowRegister] = useState(false);
-
-  // All helper functions
-  const handleCloseLogin = () => setShowLogin(false);
-  const handleShowLogin = () => setShowLogin(true);
-  const handleCloseRegister = () => setShowRegister(false);
-  const handleShowRegister = () => setShowRegister(true);
-
-  // const registerUser = () => {
-  //   axios.post('http://localhost:3004/api/users', {
-  //     username: newUserDetails.username,
-  //     first_name: newUserDetails.first_name,
-  //     last_name: newUserDetails.last_name,
-  //     email: newUserDetails.email,
-  //     password: newUserDetails.password,
-  //     avatar: newUserDetails.avatar,
-  //     city: newUserDetails.city,
-  //     country: newUserDetails.country
-  //   })
-  //     .then(res => {
-  //       console.log(res);
-  //     })
-  // }
-
-  // const updateGameConsole = () => {
-  //   console.log("YO")
-  //   setGameConsole(contents[0].content);
-  // }
+  const [loginStatus, setLoginStatus] = useState("");
 
   // To update state from db on load
   useEffect(() => {
@@ -73,14 +33,6 @@ export default function useApplicationData() {
   }, [])
 
   return {
-    showLogin,
-    setShowLogin,
-    showRegister,
-    setShowRegister,
-    handleCloseLogin,
-    handleShowLogin,
-    handleCloseRegister,
-    handleShowRegister,
     levels,
     setLevels,
     contents,
@@ -90,6 +42,8 @@ export default function useApplicationData() {
     users,
     setUsers,
     userInput,
-    setUserInput
+    setUserInput,
+    loginStatus,
+    setLoginStatus
   };
 }
