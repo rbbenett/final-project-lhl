1af1bd1309808f79adf67514751f2c652b9f9f11
saadkureshi <saq619@gmail.com>
2021-02-16 18:14:57 +0000

    local storage for user details implemented, refactor of navbar

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -3,31 +3,19 @@ import useApplicationData from "./hooks/useApplicationData"
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 // Import Components
-import Register from "./components/Register.jsx";
 import Footer from "./components/Footer.jsx";
-import Login from "./components/Login.jsx";
 import Leaderboard from "./components/Leaderboard.jsx";
 import Profile from "./components/Profile.jsx";
 import Welcomepage from "./components/WelcomePage.jsx";
 import GameConsole from "./components/GameConsole.jsx";
-
-// Import Bootstrap Components
-import { Navbar, Nav, Modal, Button, Image, Dropdown, DropdownButton } from 'react-bootstrap';
+import NavbarTC from "./components/NavbarTC.jsx";
 
 // Import stylesheets
 import './App.css';
 
 function App() {
 
-  const { 
-    showLogin, 
-    setShowLogin,
-    showRegister,
-    setShowRegister,
-    handleCloseLogin,
-    handleShowLogin,
-    handleCloseRegister,
-    handleShowRegister,
+  const {
     contents, 
     setContents,
     gameConsole,
@@ -39,56 +27,7 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Link to="/">
-            <Image href="/" src="images/typing-icon.png" className="typecraft-logo" />
-          </Link>
-          <Navbar.Brand href="/">Typecraft</Navbar.Brand>
-          <Nav className="mr-auto">
-          </Nav>
-          <Nav>
-            <Navbar.Brand>Welcome back, Bob</Navbar.Brand>
-            <Nav.Link onClick={handleShowLogin}>Login</Nav.Link>
-            <Nav.Link onClick={handleShowRegister}>Register</Nav.Link>
-            <Dropdown>
-              <Dropdown.Toggle variant={'dark'} id="dropdown-basic">
-                <Image src="images/fox.jpg" className="navbar-avatar" />
-              </Dropdown.Toggle>
-              <Dropdown.Menu className="dropdown-menu-container">
-                <Dropdown.Item href="/profile">View Profile</Dropdown.Item>
-                <Dropdown.Item href="/leaderboard">View Leaderboard</Dropdown.Item>
-                <Dropdown.Item href="/">Logout</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Nav>
-        </Navbar>
-
-        {/* Modal for Login Form */}
-        <Modal show={showLogin} onHide={handleCloseLogin}>
-          <Modal.Header closeButton>
-            <Modal.Title>Login</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Login />
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" onClick={handleCloseLogin}>
-              Forgot Username or Password?
-            </Button>
-          </Modal.Footer>
-        </Modal>
-
-        {/* Modal for Register Form */}
-        <Modal show={showRegister} onHide={handleCloseRegister}>
-          <Modal.Header closeButton>
-            <Modal.Title>Register</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Register />
-          </Modal.Body>
-        </Modal>
-
-
+        <NavbarTC />
         <Switch>
           <Route path="/leaderboard">
             <Leaderboard />
@@ -110,4 +49,5 @@ function App() {
     </Router>
   );
 }
+
 export default App;
