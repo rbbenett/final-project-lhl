a9226d25009fdf73e14910ceae01e0d18d786723
Robert <rbbenett0614@gmail.com>
2021-02-16 21:09:34 -0500

    Updated welcome page

--- final-project/src/components/WelcomePage.jsx
+++ final-project/src/components/WelcomePage.jsx
@@ -1,45 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Jumbotron, Image, Button, Carousel } from 'react-bootstrap';
 import useSound from 'use-sound';
 import DelayLink from 'react-delay-link';
 import "./WelcomePage.css";
+import Leaderboard from "./Leaderboard"
+
 
 function Welcomepage() {
   const soundUrl = "./sounds/roll.mp3";
 
   const [play] = useSound(soundUrl);
 
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
     <div className="welcome-container">
       <Jumbotron fluid>
-        <Carousel>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="holder.js/800x400?text=First slide&bg=373940"
+              src="./images/rick.jpg"
               alt="First slide"
             />
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <Image src="images/typing-icon.png" className="typing-icon" />
+              <h1>Welcome to Typecraft!</h1>
+              <p>
+                Before you start playing, here's how the game is going to work:
+              </p>
+              <ul>
+                1. A selection of words will appear on the screen
+              </ul>
+              <ul>
+                2. You have to type the worst as fast as possible!
+              </ul>
+              <ul>
+                3. But, you wont be able to advance on to the next level if there are any mistakes so make sure you check for mistakes as well
+              </ul>
+              <ul>
+                4. As the levels go up the time allocated to each level will stay the same but the amount of words will get bigger.
+              </ul>
+              <ul>
+                5. When you run out of time without having finished typing your paragraph, then Game Over
+              </ul>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
+              src="./images/rick.jpg"
               alt="Second slide"
             />
 
             <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <Leaderboard />
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="holder.js/800x400?text=Third slide&bg=20232a"
+              src="./images/rick.jpg"
               alt="Third slide"
             />
 
@@ -49,32 +74,9 @@ function Welcomepage() {
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
-        {/* <Container>
-          <Image src="images/typing-icon.png" className="typing-icon" />
-          <h1>Welcome to Typecraft!</h1>
-          <p>
-            Before you start playing, here's how the game is going to work:
-          </p>
-          <ul>
-            A selection of words will appear on the screen
-          </ul>
-          <ul>
-            You have to type the worst as fast as possible!
-          </ul>
-          <ul>
-            But, you wont be able to advance on to the next level if there are any mistakes so make sure you check for mistakes as well
-          </ul>
-          <ul>
-            As the levels go up the time allocated to each level will stay the same but the amount of words will get bigger.
-          </ul>
-          <ul>
-            When you run out of time without having finished typing your paragraph, then Game Over
-          </ul>
-
-        </Container> */}
-         <DelayLink delay={3000} to="/play" clickAction={play} replace={false}>
-        <Button variant="primary" size="lg">Play Game</Button>
-      </DelayLink>
+        <DelayLink delay={3000} to="/play" clickAction={play} replace={false}>
+          <Button className="play-game" variant="primary" size="lg">Play Game</Button>
+        </DelayLink>
       </Jumbotron>
     </div>
   )
