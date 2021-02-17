8b5c6929670b30ca6a8bc2ad5304c56eca5841a8
Robert <rbbenett0614@gmail.com>
2021-02-16 20:07:50 -0500

    Working on homepage

--- final-project/src/components/WelcomePage.jsx
+++ final-project/src/components/WelcomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Jumbotron, Image, Button } from 'react-bootstrap';
+import { Container, Jumbotron, Image, Button, Carousel } from 'react-bootstrap';
 import useSound from 'use-sound';
 import DelayLink from 'react-delay-link';
 import "./WelcomePage.css";
@@ -12,7 +12,44 @@ function Welcomepage() {
   return (
     <div className="welcome-container">
       <Jumbotron fluid>
-        <Container>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=First slide&bg=373940"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Second slide&bg=282c34"
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=20232a"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+        {/* <Container>
           <Image src="images/typing-icon.png" className="typing-icon" />
           <h1>Welcome to Typecraft!</h1>
           <p>
@@ -33,10 +70,11 @@ function Welcomepage() {
           <ul>
             When you run out of time without having finished typing your paragraph, then Game Over
           </ul>
-          <DelayLink delay={3000} to="/play" clickAction={play} replace={false}>
-            <Button variant="primary" size="lg">Play Game</Button>
-          </DelayLink>
-        </Container>
+
+        </Container> */}
+         <DelayLink delay={3000} to="/play" clickAction={play} replace={false}>
+        <Button variant="primary" size="lg">Play Game</Button>
+      </DelayLink>
       </Jumbotron>
     </div>
   )
