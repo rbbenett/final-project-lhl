d1d6c99499ae450fe36a99dddfbdd251788b076c
Robert <rbbenett0614@gmail.com>
2021-02-18 16:27:11 -0500

    Working on map feature

---
+++ final-project/src/components/PlayerMap.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import useApplicationData from "../hooks/useApplicationData"
+
+const containerStyle = {
+  width: '600px',
+  height: '600px'
+};
+
+const center = {
+  lat: 43.6532,
+  lng: -79.3832
+};
+
+const position = {
+  lat: 37.772,
+  lng: -122.214
+}
+
+function Map() {
+  
+  const { users, setUsers } = useApplicationData()
+
+  const userLocations = () => {
+    let userLocation = {
+      name: "",
+      location: {
+        lat: 0,
+        lng: 0
+      },
+    }
+    for (let i = 0; i < users.length; i++ ) {
+      userLocation = {
+        name: users[i].username,
+        location: {
+          lat: users[i].city,
+          long: users[i].country
+        }
+      } 
+      return userLocation
+    }
+  }
+
+  console.log(userLocations())
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDTS3yHuTm7IXgD_uR3OkLeEFriQA6pWwQ"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default React.memo(Map)
---
+++ final-project/src/hooks/useMap.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+
+const options = {
+  zoomControlOptions: {
+    position: google.maps.ControlPosition.RIGHT_CENTER // 'right-center' ,
+    // ...otherOptions
+  }
+}
+
+function MyComponent() {
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyDTS3yHuTm7IXgD_uR3OkLeEFriQA6pWwQ" // ,
+    // ...otherOptions
+  })
+
+  const renderMap = () => {
+    // wrapping to a function is useful in case you want to access `window.google`
+    // to eg. setup options or create latLng object, it won't be available otherwise
+    // feel free to render directly if you don't need that
+    const onLoad = React.useCallback(
+      function onLoad (mapInstance) {
+        // do something with map Instance
+      }
+    )
+    return <GoogleMap
+      options={options}
+      onLoad={onLoad}
+    >
+      {
+        // ...Your map components
+      }
+    </GoogleMap>
+  }
+
+  if (loadError) {
+    return <div>Map cannot be loaded right now, sorry.</div>
+  }
+
+  return isLoaded ? renderMap() : <Spinner />
+}
