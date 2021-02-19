1352f5887e8161ebcc24e9edca1c160a6ef8990c
saadkureshi <saq619@gmail.com>
2021-02-19 04:21:58 +0000

    pull data from random.me api implemented

--- final-project/src/setupProxy.js
+++ final-project/src/setupProxy.js
@@ -10,6 +10,9 @@ module.exports = function (app) {
   app.use(
     createProxyMiddleware("/attempts", { target: "http://localhost:3004/" })
   );
+  app.use(
+    createProxyMiddleware("/contents", { target: "http://localhost:3004/" })
+  );
 };
 
 // "proxy": {
