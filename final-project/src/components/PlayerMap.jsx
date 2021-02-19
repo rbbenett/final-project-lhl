678296354082498ed3fa2b6834b0df619c39613e
Robert <rbbenett0614@gmail.com>
2021-02-18 23:04:40 -0500

    Cleaned up map code

--- final-project/src/components/PlayerMap.jsx
+++ final-project/src/components/PlayerMap.jsx
@@ -1,36 +1,19 @@
 import React, { useState } from 'react'
 import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
-import Geocode from "react-geocode";
-
-const containerStyle = {
-  width: '600px',
-  height: '600px'
-};
-
-const center = {
-  lat: 43.6532,
-  lng: -79.3832
-};
-
-const position = {
-  lat: 37.772,
-  lng: -122.214
-}
-
-// set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
-Geocode.setApiKey(process.env.REACT_APP_GOOGLE_API_KEY);
-
-// set location_type filter . Its optional.
-// google geocoder returns more that one address for given lat/lng.
-// In some case we need one address as response for which google itself provides a location_type filter.
-// So we can easily parse the result for fetching address components
-// ROOFTOP, RANGE_INTERPOLATED, GEOMETRIC_CENTER, APPROXIMATE are the accepted values.
-// And according to the below google docs in description, ROOFTOP param returns the most accurate result.
-Geocode.setLocationType("ROOFTOP");
 
 function Map() {
 
   const [ selected, setSelected ] = useState({});
+
+  const center = {
+    lat: 43.6532,
+    lng: -79.3832
+  };
+
+  const containerStyle = {
+    width: '600px',
+    height: '600px'
+  };
   
   const onSelect = item => {
     setSelected(item);
@@ -108,8 +91,6 @@ function Map() {
       },
     }
   ];
-
-
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
