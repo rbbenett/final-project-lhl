17bb4f6487c42fd558a62216dba97a6d427bdb36
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-02-22 11:22:03 -0500

    small styling on tables changed

--- final-project/src/components/Leaderboard.jsx
+++ final-project/src/components/Leaderboard.jsx
@@ -33,12 +33,12 @@ export default function Leaderboard() {
   return (
     <div className="leaderboard">
       <Container className="leadercontainer">
-        <Table striped bordered hover variant="dark">
+        <Table  bordered >
           <thead>
-            <tr>
+            <tr className="firstrow">
               <th colSpan="4" className="align-me">Leaderboard</th>
             </tr>
-            <tr>
+            <tr className="secondrow">
               <th>Ranking</th>
               <th>Username</th>
               <th>Highest Level</th>
@@ -46,61 +46,61 @@ export default function Leaderboard() {
             </tr>
           </thead>
           <tbody>
-            <tr>
+            <tr className="firstrow">
               <td>1st</td>
               <td>@{sortedUsers[0] && sortedUsers[0].username}</td>
               <td>{sortedUsers[0] && sortedUsers[0].highest_level_cleared}</td>
               <td>{sortedUsers[0] && sortedUsers[0].words_per_min}</td>
             </tr>
-            <tr>
-              <td>2nd</td>
+            <tr className="secondrow">
+              <td >2nd</td>
               <td>@{sortedUsers[1] && sortedUsers[1].username}</td>
               <td>{sortedUsers[1] && sortedUsers[1].highest_level_cleared}</td>
               <td>{sortedUsers[1] && sortedUsers[1].words_per_min}</td>
             </tr>
-            <tr>
+            <tr className="firstrow">
               <td>3rd</td>
               <td>@{sortedUsers[2] && sortedUsers[2].username}</td>
               <td>{sortedUsers[2] && sortedUsers[2].highest_level_cleared}</td>
               <td>{sortedUsers[2] && sortedUsers[2].words_per_min}</td>
             </tr>
-            <tr>
+            <tr className="secondrow">
               <td>4th</td>
               <td>@{sortedUsers[3] && sortedUsers[3].username}</td>
               <td>{sortedUsers[3] && sortedUsers[3].highest_level_cleared}</td>
               <td>{sortedUsers[3] && sortedUsers[3].words_per_min}</td>
             </tr>
-            <tr>
+            <tr className="firstrow">
               <td>5th</td>
               <td>@{sortedUsers[4] && sortedUsers[4].username}</td>
               <td>{sortedUsers[4] && sortedUsers[4].highest_level_cleared}</td>
               <td>{sortedUsers[4] && sortedUsers[4].words_per_min}</td>
             </tr>
-            <tr>
+            <tr className="secondrow">
               <td>6th</td>
               <td>@{sortedUsers[5] && sortedUsers[5].username}</td>
               <td>{sortedUsers[5] && sortedUsers[5].highest_level_cleared}</td>
               <td>{sortedUsers[5] && sortedUsers[5].words_per_min}</td>
             </tr>
-            <tr>
+            <tr className="firstrow">
               <td>7th</td>
               <td>@{sortedUsers[6] && sortedUsers[6].username}</td>
               <td>{sortedUsers[6] && sortedUsers[6].highest_level_cleared}</td>
               <td>{sortedUsers[6] && sortedUsers[6].words_per_min}</td>
             </tr>
-            <tr>
+            <tr className="secondrow">
               <td>8th</td>
               <td>@{sortedUsers[7] && sortedUsers[7].username}</td>
               <td>{sortedUsers[7] && sortedUsers[7].highest_level_cleared}</td>
               <td>{sortedUsers[7] && sortedUsers[7].words_per_min}</td>
             </tr>
-            <tr>
+            <tr className="firstrow">
               <td>9th</td>
               <td>@{sortedUsers[8] && sortedUsers[8].username}</td>
               <td>{sortedUsers[8] && sortedUsers[8].highest_level_cleared}</td>
               <td>{sortedUsers[8] && sortedUsers[8].words_per_min}</td>
             </tr>
-            <tr>
+            <tr className="secondrow">
               <td>10th</td>
               <td>@{sortedUsers[9] && sortedUsers[9].username}</td>
               <td>{sortedUsers[9] && sortedUsers[9].highest_level_cleared}</td>
