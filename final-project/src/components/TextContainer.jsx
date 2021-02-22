1d751b4a6c0194663fac4e5547c517abf09379e2
saadkureshi <saq619@gmail.com>
2021-02-22 01:48:25 +0000

    typist vs typists fixed

--- final-project/src/components/TextContainer.jsx
+++ final-project/src/components/TextContainer.jsx
@@ -5,7 +5,8 @@ import './TextContainer.css';
 export default function TextContainer({ users }) {
   return (
     <div className="outermost-container">
-      <p className="number-online">{users.length} Typists Online</p>
+      {/* <p className="number-online">{users.length} Typists Online</p> */}
+      <p className="number-online">{users.length}{users.length > 1 ? " typists online" : " typist online"}</p>
       <div className="textContainer">
         {users.map(({ name }) => (
           <p key={name} className="each-online-user">
