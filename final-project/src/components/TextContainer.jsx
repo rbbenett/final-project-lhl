8b15be8648400dfb89117e8b32888c032b9fcb80
saadkureshi <saq619@gmail.com>
2021-02-22 17:58:06 +0000

    timer font size increased

--- final-project/src/components/TextContainer.jsx
+++ final-project/src/components/TextContainer.jsx
@@ -5,12 +5,7 @@ import './TextContainer.css';
 export default function TextContainer({ users }) {
   return (
     <div className="outermost-container">
-<<<<<<< HEAD
       <p className="number-online">{users.length}{users.length > 1 ? " typists online" : " typist online"}</p>
-=======
-      {/* <p className="number-online">{users.length} Typists Online</p> */}
-      <p className="number-online">{users.length}{users.length > 1 ? " Typists Online" : " Typist Online"}</p>
->>>>>>> 2a8b7861a564e77e303e52581b78a25d91f3d094
       <div className="textContainer">
         {users.map(({ name }) => (
           <p key={name} className="each-online-user">
