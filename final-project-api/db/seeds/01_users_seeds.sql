70e63d8bc338a0a40ee49617e15980735e0dfbb5
Robert <rbbenett0614@gmail.com>
2021-02-21 22:55:19 -0500

    Updated seeds

--- final-project-api/db/seeds/01_users_seeds.sql
+++ final-project-api/db/seeds/01_users_seeds.sql
@@ -1,12 +1,12 @@
 INSERT INTO users (username, first_name, last_name, email, password, avatar, city, country, words_per_min, highest_level_cleared)
 VALUES 
 ('Jsmith', 'Jaden', 'Smith', 'jsmith@gmail.com', 'password', 'Dinosaur', 'Toronto', 'Canada', 66.73, 5),
-('ChuckNorr', 'Chuck', 'Norris', 'ChuckNorris@hotmail.com', 'redhead', 'Monster', 'Los Angeles', 'USA', 43.42, 3),
+('ChuckNorr', 'Chuck', 'Norris', 'ChuckNorris@hotmail.com', 'redhead', 'Monster', 'Dallas', 'USA', 43.42, 3),
 ('Tfey', 'Tina', 'Fey', 'TinaFey@gmail.com', '30rock', 'Unicorn', 'New York City', 'USA', 12, 0),
-('CChanning', 'Carol', 'Channing', 'carol@channing.ca', 'outofmymind', 'Unicorn', 'Montreal', 'Canada', 39.85, 3),
+('CChanning', 'Carol', 'Channing', 'carol@channing.ca', 'outofmymind', 'Unicorn', 'Seattle', 'USA', 39.85, 3),
 ('Cdion', 'Celine', 'Dion', 'Celine@dion.ca', 'Myheartwill', 'Unicorn', 'Montreal', 'Canada', 102.90, 10),
-('WGretzsky', 'Wayne', 'Gretzsky', 'wgretzsky@hockey.com', 'pucks', 'Ghost', 'Toronto', 'Canada', 25, 5),
+('WGretzky', 'Wayne', 'Gretzsky', 'wgretzsky@hockey.com', 'pucks', 'Ghost', 'Brantford', 'Canada', 25, 5),
 ('JTrudeau', 'Justin', 'Trudeau', 'pm@canada.gov', 'maple', 'Dinosaur', 'Ontario', 'Canada', 350, 10),
 ('CTatum', 'Channing', 'Tatum', '21jumpstreet@la.gov', 'hotstuff', 'Monster', 'Los Angeles', 'USA', 33.68, 2),
 ('MMouse', 'Mickey', 'Mouse', 'Mouse@disney.com', 'hifolks', 'Unicorn', 'Orlando', 'Florida', 68.77, 8),
-('CLloyd', 'Christopher', 'Lloyd', 'greatscott@fox.com', '88mph', 'Ghost', 'Pasadena', 'USA', 99.88, 7)
+('CLloyd', 'Christopher', 'Lloyd', 'greatscott@fox.com', '88mph', 'Ghost', 'Petaluma', 'USA', 99.88, 7)
