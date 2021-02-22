3edb9fb5373d69158cc4b6a7a1f9f094db2aaffe
saadkureshi <saq619@gmail.com>
2021-02-22 03:13:36 +0000

    some styling added

--- final-project/src/components/TextContainer.jsx
+++ final-project/src/components/TextContainer.jsx
@@ -5,7 +5,6 @@ import './TextContainer.css';
 export default function TextContainer({ users }) {
   return (
     <div className="outermost-container">
-      {/* <p className="number-online">{users.length} Typists Online</p> */}
       <p className="number-online">{users.length}{users.length > 1 ? " typists online" : " typist online"}</p>
       <div className="textContainer">
         {users.map(({ name }) => (
