12b5cccb8dfbc911f0a4dadf0ff810501a19f46d
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-02-23 16:10:31 -0500

    fixed the posting to users bug

--- final-project/src/components/GameConsole.jsx
+++ final-project/src/components/GameConsole.jsx
@@ -8,7 +8,7 @@ import "./GameConsole.css"
 
 export default function GameConsole(props) {
 
-  const { attempts, levels } = useApplicationData()
+  const { attempts, setAttempts, levels } = useApplicationData()
 
   const [seconds, setSeconds] = useState(30);
   const [typingIn, setTypingIn] = useState("");
@@ -23,13 +23,17 @@ export default function GameConsole(props) {
   const currentUser = (localStorage.getItem("user_details") && JSON.parse(localStorage.getItem("user_details"))?.id)
 
   // calculate wpm of the user
-  const totalAvgWpm = function () {
+  const totalAvgWpm = function (wordsDone, timeTaken) {
     let result = []
+    const newValue = {}
+    newValue.words_completed = wordsDone
+    newValue.time_taken = timeTaken
     for (let attempt of attempts) {
       if (attempt.user_id === currentUser) {
         result.push(attempt)
       }
     }
+    result = [...result, newValue]
     let totalWords = (result.reduce((a, b) => a + (parseInt(b.words_completed) || 0), 0))
     let totalTime = result.reduce((a, b) => a + (parseInt(b.time_taken) || 0), 0) / 60
     return totalWords / totalTime
@@ -74,8 +78,6 @@ export default function GameConsole(props) {
       setSeconds("Game Over");
     }
   }
-
-  console.log(levels)
 
   // use randomtext api to get random sentences
   const giveMeRandomText = (level_id) => {
@@ -166,7 +168,7 @@ export default function GameConsole(props) {
       setSeconds(30)
       Timer(30)
     }
-    if ((currentLevel) > highestLevel) {
+    if (currentLevel > highestLevel) {
       setHighestLevel(currentLevel)
     }
   }, [currentLevel])
@@ -178,7 +180,7 @@ export default function GameConsole(props) {
       setLevelStarted(false)
       let currentLevelWords = props.contents[currentLevel].content.split(' ')
       let totalOfCorrectWords = totalWordsCorrect(typingIn, currentLevelWords)
-      let wpm = totalAvgWpm()
+      let wpm = totalAvgWpm(totalOfCorrectWords, 30)
       setLevelContent("Game Over")
       clearInterval(intervalId)
       setTypingIn("");
@@ -205,7 +207,6 @@ export default function GameConsole(props) {
       let correctWords = text.split(' ').length;
       let secondsLeft = 30 - seconds;
       clearInterval(intervalId);
-      console.log((currentLevel + 1), JSON.parse(localStorage.getItem("user_details"))?.highest_level_cleared)
       setCurrentLevel(currentLevel + 1)
       setSeconds(30)
       setTypingIn("");
@@ -216,14 +217,18 @@ export default function GameConsole(props) {
         time_taken: secondsLeft,
         passed: true,
         current_highest_level_passed: JSON.parse(localStorage.getItem("user_details"))?.highest_level_cleared,
-        wpm: totalAvgWpm()
       })
         .then(res => {
           axios.post("/users", {
             user_id: JSON.parse(localStorage.getItem("user_details"))?.id,
             level_id: currentLevel + 1,
-            wpm: totalAvgWpm(),
+            wpm: totalAvgWpm(correctWords, secondsLeft),
             current_highest_level_passed: JSON.parse(localStorage.getItem("user_details"))?.highest_level_cleared
+          })
+          .then(res => {
+            if (currentLevel > highestLevel) {
+              setHighestLevel(currentLevel)
+            }
           })
         })
         .catch(err => console.log(err))
